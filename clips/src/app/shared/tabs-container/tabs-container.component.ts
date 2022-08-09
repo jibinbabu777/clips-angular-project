7105d9840b639a4ab43988b76b2e0d02835a901c
jibinbabukummil@gmail.com <jibinbabukummil@gmail.com>
2022-08-10 00:45:41 +0530

    tabs added

--- clips/src/app/shared/tabs-container/tabs-container.component.ts
+++ clips/src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
+//Use to get the QueryList of elements or directives from the content DOM
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> 
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterContentInit(): void {
+      const activeTabs = this.tabs?.filter(
+        tab => tab.active
+      )
 
+      if(!activeTabs || activeTabs.length===0){
+        this.selectTabs(this.tabs!.first)
+      }
+  }
+  selectTabs(tab: TabComponent){
+    this.tabs?.forEach(
+      tab=> tab.active =false
+    )
+    tab.active=true;
+    //preventing default behaviour
+    return false;
+  }
 }
