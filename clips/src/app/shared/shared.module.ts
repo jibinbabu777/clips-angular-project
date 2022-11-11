f326a1987abe2f51a95ad78ca72689ca71007a1c
jibinbabukummil@gmail.com <jibinbabukummil@gmail.com>
2022-11-11 23:01:55 +0530

    upload component

--- clips/src/app/shared/shared.module.ts
+++ clips/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './alert/alert.component';
+import { EventBlockerDirective } from './directives/event-blocker.directive';
 
 
 
@@ -15,7 +16,8 @@ import { AlertComponent } from './alert/alert.component';
     TabsContainerComponent,
     TabComponent,
     InputComponent,
-    AlertComponent
+    AlertComponent,
+    EventBlockerDirective
   ],
   imports: [
     CommonModule,
@@ -26,7 +28,8 @@ import { AlertComponent } from './alert/alert.component';
     TabComponent,
     TabsContainerComponent,
     InputComponent,
-    AlertComponent
+    AlertComponent,
+    EventBlockerDirective
   ]
 })
 export class SharedModule { }
